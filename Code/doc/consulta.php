9fdf7c20ecf4964280a29474f4c4e895f0d3ef64
DilanZuniga <dylankarim96@gmail.com>
2016-04-22 17:16:12 -0600

    html
    html

---
+++ Code/doc/consulta.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<!--
+	Autor: Dilan Zúniga
+-->
+<html>
+	<head>
+		<title>HU-Capital</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing3">
+
+
+
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.html">HU-Capital</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="index.html">INICIO</a></li>
+                                                                                        <li><a href="http://localhost/HU-Capital/login.php">CONSULTA</a></li>
+											<li><a href="convenios.html">Convenios</a></li>
+											<!--<li><a href="elements.html">Elements</a></li>-->
+											<!--<li><a href="#">Sign Up</a></li>-->
+											
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Banner -->
+					<section id="banner3">
+						<div class="inner">
+							<h2>CONSULTA</h2>
+							
+                                            
+<form action="http://www.aprenderaprogramar.com" method="get">
+<p>
+<label for="usuario">identificación: </label>
+<input type="text" name="usuario" id="usuario">
+<br/> <br/>
+
+
+
+<input type="reset">
+</p>
+</form>
+
+                                                         
+
+
+							<ul class="actions">
+								<li><a href="http://localhost/HU-Capital/info.html" class="button special">CONSULTAR</a></li>
+							</ul>
+						</div>
+						
+					</section>
+<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+</body>
+</html>
